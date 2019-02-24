45fdaf28730d6bf3350468b2daf9435407cebad0
Kamil Gronowski <kamil540@gazeta.pl>
2019-02-24 14:44:15 +0100

    Port to 1.13.2

--- src/main/java/pyre/followingvillagers/FollowingVillagers.java
+++ src/main/java/pyre/followingvillagers/FollowingVillagers.java
@@ -1,9 +1,14 @@
 package pyre.followingvillagers;
 
-import pyre.followingvillagers.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import pyre.followingvillagers.util.EventHandler;
+import pyre.followingvillagers.util.Reference;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+@Mod(Reference.MOD_ID)
 public class FollowingVillagers {
 
+    public FollowingVillagers() {
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
+    }
 }
--- src/main/java/pyre/followingvillagers/util/EventHandler.java
+++ src/main/java/pyre/followingvillagers/util/EventHandler.java
@@ -3,19 +3,17 @@ package pyre.followingvillagers.util;
 import net.minecraft.entity.ai.EntityAITempt;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-@Mod.EventBusSubscriber
 public class EventHandler {
 
     @SubscribeEvent
-    public static void addVillagerFollowTast(EntityJoinWorldEvent event) {
+    public void addVillagerFollowTast(final EntityJoinWorldEvent event) {
         if (!event.getWorld().isRemote && event.getEntity() instanceof EntityVillager) {
-            EntityVillager villager = (EntityVillager)event.getEntity();
-            EntityAITempt followPlayerTask = new EntityAITempt(villager, 0.6D, Item.getItemFromBlock(Blocks.EMERALD_BLOCK), false);
+            EntityVillager villager = (EntityVillager) event.getEntity();
+            EntityAITempt followPlayerTask = new EntityAITempt(villager, 0.6D, Ingredient.fromItems(Blocks.EMERALD_BLOCK.asItem()), false);
             villager.tasks.addTask(3, followPlayerTask);
         }
     }
