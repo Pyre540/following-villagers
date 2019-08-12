e4446d343de5b61aedc2f9b26d60fb0776e24749
Kamil Gronowski <kamil540@gazeta.pl>
2019-08-12 20:23:52 +0200

    Port to 1.14.4

--- src/main/java/pyre/followingvillagers/util/EventHandler.java
+++ src/main/java/pyre/followingvillagers/util/EventHandler.java
@@ -1,8 +1,8 @@
 package pyre.followingvillagers.util;
 
-import net.minecraft.entity.ai.EntityAITempt;
-import net.minecraft.entity.passive.EntityVillager;
-import net.minecraft.init.Blocks;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,10 +11,10 @@ public class EventHandler {
 
     @SubscribeEvent
     public void addVillagerFollowTast(final EntityJoinWorldEvent event) {
-        if (!event.getWorld().isRemote && event.getEntity() instanceof EntityVillager) {
-            EntityVillager villager = (EntityVillager) event.getEntity();
-            EntityAITempt followPlayerTask = new EntityAITempt(villager, 0.6D, Ingredient.fromItems(Blocks.EMERALD_BLOCK.asItem()), false);
-            villager.tasks.addTask(3, followPlayerTask);
+        if (!event.getWorld().isRemote && event.getEntity() instanceof VillagerEntity) {
+            VillagerEntity villager = (VillagerEntity) event.getEntity();
+            TemptGoal followPlayerTask = new TemptGoal(villager, 0.5D, Ingredient.fromItems(Blocks.EMERALD_BLOCK.asItem()), false);
+            villager.goalSelector.addGoal(4, followPlayerTask);
         }
     }
 }
