2d156d5b1f7d3fc6c12dc28fa01d228fa3fe5d5a
Kamil Gronowski <kamil540@gazeta.pl>
2020-02-04 21:48:42 +0100

    Port to 1.15.2. Simplified code.

--- src/main/java/pyre/followingvillagers/FollowingVillagers.java
+++ src/main/java/pyre/followingvillagers/FollowingVillagers.java
@@ -1,14 +1,27 @@
 package pyre.followingvillagers;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import pyre.followingvillagers.util.EventHandler;
-import pyre.followingvillagers.util.Reference;
 
-@Mod(Reference.MOD_ID)
+@Mod("followingvillagers")
 public class FollowingVillagers {
 
     public FollowingVillagers() {
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void addVillagerFollowTask(final EntityJoinWorldEvent event) {
+        if (!event.getWorld().isRemote && event.getEntity() instanceof VillagerEntity) {
+            VillagerEntity villager = (VillagerEntity) event.getEntity();
+            TemptGoal followPlayerTask = new TemptGoal(villager, 0.5D, Ingredient.fromItems(Blocks.EMERALD_BLOCK.asItem()), false);
+            villager.goalSelector.addGoal(4, followPlayerTask);
+        }
     }
 }
